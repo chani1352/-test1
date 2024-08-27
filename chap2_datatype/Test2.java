a0ac734c47e7ccb04b0f09936882a437bf21f9f5
chani1352 <chani1352@naver.com>
2024-08-27 13:06:28 +0900

    자바 test1

---
+++ chap2_datatype/Test2.java
@@ -0,0 +1,18 @@
+package chap2_datatype;
+
+public class Test2 {
+	public static void main(String[] args) {
+		int depositAmount;
+		depositAmount = 50000; //정수 변수
+		
+		//주석
+		/*
+		 * 변수 선언
+		 * 데이타 타입
+		 * 
+		 */
+		System.out.println(depositAmount);
+		System.out.println("출력결과");
+	}
+
+}
